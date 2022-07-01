f4b31e0b6f81fdc04fb48daf29bd80cd045e5b78
SFEYI28 <solofeyi@gmail.com>
2022-07-01 16:04:08 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *  * main program that prints all possible combinations of single-digit
+ *  * main - program that prints all possible combinations of single-digit
  *   * Numbers must be separated by ,followed by a space
  *    * Numbers should be printed in ascending order
  *     * You can only use the putchar function
