f63aaa1729ebfa7c6d42cfe1961332d90ca0ba0a
SFEYI28 <solofeyi@gmail.com>
2022-07-06 02:23:49 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,26 @@
+#include <stdio.h>
 #include "main.h"
 
-/** main - prints putchar	
- *  Return: Always 0 (Success)
- **/
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+/**
+ *  * main - Entry point
+ *   * Description: prints 'Holberton \n'
+ *    * Return: Always 0 (success)
+ *     */
 
-	return (0);
+int main(void)
+
+{
+
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+
+		char c = str[i];
+		_putchar(c);
+		i++;
+	}
+		return (0);
 
 }
