894d0055a4a5fdca5b25b6c04df31dad084becb3
SFEYI28 <solofeyi@gmail.com>
2022-07-07 16:07:57 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,12 +3,12 @@
 /**
  * main - causes an infinite loop
  * Return: 0
-*/
+ */
 
 int main(void)
 {
 int i;
-printf("Infinite loop incoming: (\n");
+printf("Infinite loop incoming :(\n");
 
 i = 0;
 
@@ -17,8 +17,7 @@ i = 0;
  * {
  * putchar(i);
  * }
-*/
+ */
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
