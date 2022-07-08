7ad39d71c025a82decd3bf812c9cd686f8998496
SFEYI28 <solofeyi@gmail.com>
2022-07-07 18:09:16 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,14 @@
 */
 void print_diagonal(int n)
 {
-int postn, space;
+int x;
+
 if (n <= 0)
 _putchar('\n');
 else
 {
-for (postn = 1; postn <= n; ++postn)
-{
-for (space = 1; space <= postn; ++space)
-_putchar(' ');
-_putchar(92); /*is equal to '/' char*/
+for (x = 0; x < n; x++)
+_putchar('\');
 _putchar('\n');
-}
 }
 }
